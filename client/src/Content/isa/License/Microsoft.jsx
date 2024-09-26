e1b22cb66f0f1e69cc13fce49ff74ba57abd909e
abdlrhmnrsyd <abdlrhmnrsyd@outlook.com>
2024-09-26 21:06:14 +0700

    fix button

--- client/src/Content/isa/License/Microsoft.jsx
+++ client/src/Content/isa/License/Microsoft.jsx
@@ -1062,20 +1062,20 @@ const MicrosoftComponent = () => {
                 </Table>
               </div>
             </div>
-          </div>
-          <div className="absolute bottom-0 left-0 right-0 z-50 flex items-center justify-center mb-4">
-            <button
-              onClick={scrollLeft} // Attach scrollLeft function
-              className="px-4 py-2 mr-2 text-white bg-gray-300 rounded-md hover:bg-gray-600" // Added margin-right
-            >
-              <ChevronLeft size={18} />
-            </button>
-            <button
-              onClick={scrollRight} // Attach scrollRight function
-              className="px-4 py-2 text-white bg-gray-300 rounded-md hover:bg-gray-600"
-            >
-              <ChevronRight size={18} />
-            </button>
+            <div className="flex items-center justify-center mt-0">
+              <button
+                onClick={scrollLeft} // Attach scrollLeft function
+                className="px-4 py-2 mr-2 text-white bg-gray-300 rounded-md hover:bg-gray-600" // Added margin-right
+              >
+                <ChevronLeft size={18} />
+              </button>
+              <button
+                onClick={scrollRight} // Attach scrollRight function
+                className="px-4 py-2 text-white bg-gray-300 rounded-md hover:bg-gray-600"
+              >
+                <ChevronRight size={18} />
+              </button>
+            </div>
           </div>
         </div>
      
